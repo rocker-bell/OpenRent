92485dc4096f8039732e86a41d62c4e6a552c11c
Anass Tantane <rockbell8@gmail.com>
2026-02-17 02:01:51 +0100

    Add First layout

---
+++ src/Components/LandingPage.jsx
@@ -0,0 +1,14 @@
+import "../Styles/LandingPage.css";
+
+export default function LandingPage () {
+    return (
+        <>
+
+            <div className="LandingPage_wrapper">
+                <h1>Welcome to Landing Page!</h1>
+
+            </div>
+            
+        </>
+    )
+}
